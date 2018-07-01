1379bfb878d6457833c7fc7277b86ea00f325f9b
Дом <@192.168.0.20>
2018-07-01 13:40:29 +0700

    Synchronized Code block

---
+++ Synchronized-Code-block/src/ru/dom/Worker.java
@@ -0,0 +1,85 @@
+package ru.dom;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Worker {
+	private ArrayList<Integer> list1 = new ArrayList<>();
+	private ArrayList<Integer> list2 = new ArrayList<>();
+	Random random = new Random();
+	private Object lock1 = new Object();
+	private Object lock2 = new Object();
+	
+	private void partOne() {
+		synchronized (lock1) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			list1.add(random.nextInt(100));
+		}		
+	}
+	
+	private void partTwo() {
+		synchronized (lock2) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+			list2.add(random.nextInt(100));
+		}			
+	}
+	
+	private void proceed() {
+		for(int i=0;i<1000;i++) {
+			partOne();
+			partTwo();
+		}
+	}
+	
+	public void start() {
+		System.out.println("Начинаем...");
+		long starTime = System.currentTimeMillis();
+				
+		Thread t1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				proceed();
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				proceed();
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		long endTime = System.currentTimeMillis();
+		System.out.println("Потраченное время: "+(endTime-starTime)+"\n"
+				+"Лист1: "+list1.size()+"\n"
+				+"Лист2: "+list2.size()+"\n");
+	}
+	public Worker() {
+
+			
+	}
+
+}
